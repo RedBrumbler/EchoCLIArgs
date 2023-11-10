73de63208b212c92f91df71c09f98d8e767938ad
RedBrumbler <redbrumbler.official+github@gmail.com>
2023-11-10 23:40:40 +0100

    Remove limit on length of args

--- include/ReplacementArgs.hpp
+++ include/ReplacementArgs.hpp
@@ -6,7 +6,5 @@ namespace EchoCLIArgs {
 struct ReplacementArgs {
   static std::string read_args();
   static void install_hook();
-
-  static bool apply_args(char* game_args);
 };
 }  // namespace EchoCLIArgs
--- src/ReplacementArgs.cpp
+++ src/ReplacementArgs.cpp
@@ -13,18 +13,28 @@
 #include "flamingo/shared/trampoline.hpp"
 
 namespace EchoCLIArgs {
+using namespace std::string_view_literals;
+
 uint32_t* get_target_address() {
-  auto setcommandlineargs =
-      dlsym(modloader_r15_handle, "_ZN10NRadEngine8CSysJava7GetArgsERNS_13CFixedStringTILy256EEE");
-  if (!setcommandlineargs) {
-    LOG_ERROR(
-        "Could not find symbol "
-        "_ZN10NRadEngine8CSysJava7GetArgsERNS_13CFixedStringTILy256EEE "
-        "in libr15.so! can't provide hook address");
+  static auto constexpr symbol = "_ZN10NRadEngine5Main_EPKc"sv;
+  auto engine_main = dlsym(modloader_r15_handle, symbol.data());
+  if (!engine_main) {
+    LOG_ERROR("Could not find symbol '{}' in libr15.so! can't provide hook address", symbol);
   }
 
-  return static_cast<uint32_t*>(setcommandlineargs);
+  return static_cast<uint32_t*>(engine_main);
 }
+
+char const** get_args_address() {
+  static constexpr auto offset = 0x372c000;
+  auto base = Util::baseAddr("libr15-original.so");
+  if (!base) {
+    LOG_WARN("Could not get base address for libr15-original.so, not returning valid args address!");
+    return nullptr;
+  }
+  return (char const**)(base + offset);
+}
+
 void ReplacementArgs::install_hook() {
   // find target symbol
   auto target = get_target_address();
@@ -38,43 +48,33 @@ void ReplacementArgs::install_hook() {
   trampoline.WriteCallback(&target[4]);
   trampoline.Finish();
 
-  static auto args_hook = [](void* self, char* args) noexcept {
-    // call into apply args to override args. if this is not successful just run
-    // orig
-    if (!apply_args(args)) {
+  static auto main_hook = [](char const* cli_args) noexcept {
+    // get the replacement args
+    static auto replacement_args = read_args();
+
+    // if they are not empty, apply them
+    if (!replacement_args.empty()) {
+      LOG_DEBUG("Replacing cli arguments with: '{}'", replacement_args);
+      // get the static address of the cli args pointer in r15 and override if found
+      auto args_address = get_args_address();
+      if (args_address) *args_address = replacement_args.c_str();
+      // override args passed
+      cli_args = replacement_args.c_str();
+    } else {
       LOG_WARN("custom arguments not found, so we have not set them");
-      reinterpret_cast<void (*)(void*, char*)>(trampoline.address.data())(self, args);
     }
+
+    // call orig
+    reinterpret_cast<void (*)(char const*)>(trampoline.address.data())(cli_args);
   };
 
   std::size_t trampoline_size = 64;
   std::size_t num_insts = 8;
   auto target_hook = flamingo::Trampoline(target, num_insts, trampoline_size);
-  target_hook.WriteCallback(reinterpret_cast<uint32_t*>(+args_hook));
+  target_hook.WriteCallback(reinterpret_cast<uint32_t*>(+main_hook));
   target_hook.Finish();
 
   Util::protect(target, PROT_READ | PROT_EXEC);
-}
-
-bool ReplacementArgs::apply_args(char* game_args) {
-  // read the args, store their result somewhere and apply that value
-  auto args = read_args();
-  if (args.empty()) return false;
-
-  LOG_DEBUG("Found replacement args: {}", args);
-
-  // the game_args array is max 0x100 bytes (256), taking the terminating byte
-  // into account that is 255 chars allowed
-  auto len = std::min(args.size(), 255ul);
-  if (len != args.size()) {
-    LOG_WARN(
-        "Args were of length {}, but the maximum allowed is 255. args "
-        "will be forcibly truncated!",
-        args.size());
-  }
-
-  std::memcpy(reinterpret_cast<void*>(game_args), reinterpret_cast<void const*>(args.c_str()), len);
-  return true;
 }
 
 std::string ReplacementArgs::read_args() {
